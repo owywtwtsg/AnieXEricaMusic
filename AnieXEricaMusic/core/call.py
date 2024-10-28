09d47fd9c987a7c1b08581b1931a7bb000750cab
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-29 04:38:40 +0530

    Update call.py

--- AnieXEricaMusic/core/call.py
+++ AnieXEricaMusic/core/call.py
@@ -14,6 +14,7 @@ import os
 from datetime import datetime, timedelta
 from typing import Union
 from pyrogram import Client
+from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardMarkup
 import config
 from AnieXEricaMusic import LOGGER, YouTube, app
