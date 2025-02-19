0c6312a5a747e7d59c76919ebe9c965b827f3a22
owywtwtsg <testgrpmusi@gmail.com>
2025-02-20 00:42:20 +0530

    Update bot.py

--- AnieXEricaMusic/core/bot.py
+++ AnieXEricaMusic/core/bot.py
@@ -37,19 +37,19 @@ class AMBOT(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-            exit()
+            pass
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
-            exit()
+            pass
 
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
-            exit()
+            pass
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
